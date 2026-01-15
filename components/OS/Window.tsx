cd4fccc4fa4014273b450326399d9ebc47c42dac
rotanpuolikas <tuukka.alatalo@gmail.com>
2026-01-15 15:42:46 +0200

    fixed window snapping + some removal of old non-working fix

--- components/OS/Window.tsx
+++ components/OS/Window.tsx
@@ -13,39 +13,69 @@ type WindowProps = {
   program: WindowedProgramClass
   taskID: number
 }
-const getWindowStyle = (p: v2, s: v2, z: number, max: boolean, min: boolean, moving: boolean):CSSProperties => {
-  if (!min) {
-    if (max) {
-      return {
-        transition: moving ? "none" : "width 0.3s, height 0.3s, left 0.3s, top 0.3s",
-        width: 100 + "%",
-        height: "calc(100% - var(--bar-height))",
-        left: 0 + "px",
-        top: 0 + "px",
-        zIndex: 1000 - z
-      }
-    } else {
-      return {
-        transition: moving ? "none" : "width 0.3s, height 0.3s, left 0.3s, top 0.3s",
-        width: s.x + "px",
-        height: s.y + "px",
-        left: p.x + "px",
-        top: p.y + "px",
-        zIndex:  1000 - z
-      }
-    }
-  } else {
+const getWindowStyle = (
+  p: v2,
+  s: v2,
+  z: number,
+  max: boolean,
+  min: boolean,
+  moving: boolean,
+  leftSnap: boolean,
+  rightSnap: boolean
+): CSSProperties => {
+  if (min) {
     return {
       transition: "width 0.3s, height 0.3s, left 0.3s, top 0.3s",
-      width: 0 + "%",
-      height: 0 + "%",
-      left: 25 + "%",
-      top: 100 + "%",
-      zIndex: 1000 - z
+      width: "0%",
+      height: "0%",
+      left: "25%",
+      top: "100%",
+      zIndex: 1000 - z,
     }
   }
+
+  if (max) {
+    return {
+      transition: moving ? "none" : "all 0.3s",
+      width: "100%",
+      height: "calc(100% - var(--bar-height))",
+      left: "0px",
+      top: "0px",
+      zIndex: 1000 - z,
+    }
+  }
+
+  if (leftSnap) {
+    return {
+      transition: moving ? "none" : "all 0.3s",
+      width: "50%",
+      height: "calc(100% - var(--bar-height))",
+      left: "0px",
+      top: "0px",
+      zIndex: 1000 - z,
+    }
+  }
+
+  if (rightSnap) {
+    return {
+      transition: moving ? "none" : "all 0.3s",
+      width: "50%",
+      height: "calc(100% - var(--bar-height))",
+      left: "50%",
+      top: "0px",
+      zIndex: 1000 - z,
+    }
+  }
+
+  return {
+    transition: moving ? "none" : "all 0.3s",
+    width: s.x + "px",
+    height: s.y + "px",
+    left: p.x + "px",
+    top: p.y + "px",
+    zIndex: 1000 - z,
+  }
 }
-type WindowSide = "top" | "left" | "right" | "bottom"
 
 export const ProgramWindow = ({
   position: initialPosition,
@@ -61,12 +91,13 @@ export const ProgramWindow = ({
   const [size, setSize] = useState(initialSize)
   const [windowMaximized, setWindowMaximized] = useState(false)
   const [windowMinimized, setWindowMinimized] = useState(false)
+  const [windowLeft, setWindowLeft] = useState(false)
+  const [windowRight, setWindowRight] = useState(false)
   const windowManager = useContext(WindowManagerContext)
   const [isMoving, setIsMoving] = useState(false)
   const [moveOffset, setMoveOffset] = useState({x: 0, y: 0})
-  const [resize, setResize] = useState<{o: number, side: WindowSide} | null>(null)
+  const [resize, setResize] = useState<{o: number} | null>(null)
   const cursor = useCursor()
-
   useEffect(() => {
     if (windowManager) {
       const index = windowManager.order.indexOf(taskID)
@@ -99,10 +130,54 @@ export const ProgramWindow = ({
     }
   }, [newProgram])
 
-  const windowStyle = getWindowStyle(position, size, zIndex, windowMaximized, windowMinimized, isMoving || !!resize)
+  const windowStyle = getWindowStyle(position, size, zIndex, windowMaximized, windowMinimized, isMoving || !!resize, windowLeft, windowRight)
 
-  const startMove = (e:React.MouseEvent) => {
-    setMoveOffset({x: cursor.pageX - position.x, y: cursor.pageY - position.y})
+  const startMove = (e: React.MouseEvent) => {
+    let startX = position.x
+    let startY = position.y
+    if (windowMaximized || windowLeft || windowRight) {
+      const cursorX = cursor.pageX
+      const cursorY = cursor.pageY
+
+      let snappedWidth = size.x
+      let snappedHeight = size.y
+      let snappedX = position.x
+      let snappedY = position.y
+
+      if (windowMaximized) {
+        snappedWidth = window.innerWidth
+        snappedHeight = window.innerHeight
+        snappedX = 0
+        snappedY = 0
+      } else if (windowLeft) {
+        snappedWidth = window.innerWidth / 2
+        snappedHeight = window.innerHeight
+        snappedX = 0
+        snappedY = 0
+      } else if (windowRight) {
+        snappedWidth = window.innerWidth / 2
+        snappedHeight = window.innerHeight
+        snappedX = window.innerWidth / 2
+        snappedY = 0
+      }
+
+      const relativeX = (cursorX - snappedX) / snappedWidth
+      const newX = cursorX - relativeX * size.x
+      const newY = cursorY - 10 // <- edit this number if fullscreening feels funky
+
+      startX = newX
+      startY = newY
+
+      setPosition({ x: startX, y: startY })
+      setWindowMaximized(false)
+      setWindowLeft(false)
+      setWindowRight(false)
+    }
+    setMoveOffset({
+      x: cursor.pageX - startX,
+      y: cursor.pageY - startY,
+    })
+
     setIsMoving(true)
     windowManager?.focus(taskID)
   }
@@ -118,82 +193,36 @@ export const ProgramWindow = ({
     e.stopPropagation()
     killTask(taskID)
   }
-  const doResize = (e:React.MouseEvent, side:WindowSide) => {
-    e.preventDefault()
-    e.stopPropagation()
-    switch (side) {
-      case "top":
-        setResize({o: cursor.pageY, side: side})
-        break
-      case "left":
-        setResize({o: cursor.pageX, side: side})
-        break
-      case "bottom":
-        setResize({o: cursor.pageY, side: side})
-        break
-      case "right":
-        setResize({o: cursor.pageX, side: side})
-        break
-    }
-  }
   useEffect(() => {
     if (isMoving) {
       if (cursor.clickActive) {
         const newPos = {x: cursor.pageX - moveOffset.x, y: cursor.pageY - moveOffset.y}
         if (newPos.x != position.x || newPos.y != position.y) {
-
-          if (windowMaximized) {
-            setWindowMaximized(false)
-          }
           setPosition({x: cursor.pageX - moveOffset.x, y: cursor.pageY - moveOffset.y})
         }
       } else {
-        if (position.y < 5){
+        const screenW = window.innerWidth
+        if (cursor.pageY < 10){
           setPosition({x: position.x, y: 0})
           setWindowMaximized(true)
+        }
+        else if (cursor.pageX < 5) {
+          setWindowLeft(true)
+          setWindowRight(false)
+          setWindowMaximized(false)
+        }
+    
+        else if (cursor.pageX > screenW - 5) {
+          setWindowRight(true)
+          setWindowLeft(false)
+          setWindowMaximized(false)
         }
         setIsMoving(false)
       }
     }
   }, [cursor, isMoving, moveOffset, position, windowMaximized]);
 
-  useEffect(() => {
-    if (resize) {
-      if (cursor.clickActive) {
-        const diff = {x: cursor.pageX - resize.o, y: cursor.pageY - resize.o}
-        switch (resize.side) {
-          case "top":
-            if (diff.y !== 0){
-              setResize({side: resize.side, o: cursor.pageY})
-              setPosition({x: position.x, y: position.y + diff.y})
-              setSize({x: size.x, y:size.y - diff.y})
-            }
-            break
-          case "left":
-            if (diff.x !== 0) {
-              setResize({side: resize.side, o: cursor.pageX})
-              setPosition({x: position.x + diff.x, y: position.y})
-              setSize({x: size.x - diff.x, y: size.y})
-            }
-            break
-          case "bottom":
-            if (diff.y !== 0) {
-              setResize({side: resize.side, o: cursor.pageY})
-              setSize({x: size.x, y: size.y + diff.y})
-            }
-            break
-          case "right":
-            if (diff.x !== 0) {
-              setResize({side: resize.side, o: cursor.pageX})
-              setSize({x: size.x + diff.x, y: size.y})
-            }
-            break
-        }
-      } else {
-        setResize(null)
-      }
-    }
-  }, [cursor, resize, size, position]);
+
   useEffect(() => {
     if (windowMinimized && windowManager){
       windowManager.unfocus(taskID)
@@ -202,11 +231,8 @@ export const ProgramWindow = ({
 
   return (
     <div className={styles.window} onClick={() => windowManager?.focus(taskID)} style={windowStyle}>
-      <div className={styles.windowEdgeVertical} onMouseDown={(e) => doResize(e, "left")}></div>
       <div className={styles.windowMid}>
-        <div className={styles.windowEdgeHorizontal} onMouseDown={(e) => doResize(e, "top")}></div>
 
-        <div className={styles.windowMid}>
         <div className={styles.titleBar} onMouseDown={startMove}>
           {program !== null ?
             <>
@@ -229,10 +255,7 @@ export const ProgramWindow = ({
         <div className={styles.program}>
           {program ? program.programWindow : null}
         </div>
-        </div>
-        <div className={styles.windowEdgeHorizontal} onMouseDown={(e) => doResize(e, "bottom")}></div>
       </div>
-      <div className={styles.windowEdgeVertical} onMouseDown={(e) => doResize(e, "right")}></div>
     </div>
   )
 
